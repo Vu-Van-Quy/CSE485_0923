b07b39f4f2188d19fe73935eb8b9542b1a4672de
Vu-Van-Quy <quydut0517@gmail.com>
2023-09-25 23:09:10 +0700

    1B

---
+++ BTTH01_CSE485_ex02/detail.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <?php
+                include "layout/Trangchu.php";
+            ?>
+            <div class="row">
+                <!-- <div class="main mt-5 d-flex"> -->
+                    <div class="col-md-4 ">
+                        <img src="./img/chuyen-tinh-la-gio.PNG"  class="float-right" alt="">
+                    </div>
+
+                    <div class="col-md-8">
+                        <div class="noidung col-md-6">
+                            <h3 class=" text-primary">Cây và gió</h3>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h6 class="d-inline">Bài hát:</h6>
+                                    <p class="d-inline">Cay và gió</p>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h6 class="d-inline">Thể loại:</h6>
+                                    <p class="d-inline">Nhạc trữ tình</p>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h6 class="d-inline">Tóm tắt:</h6>
+                                    <p class="d-inline">Anh và em yêu nhau thật tình cờ.Louiwf bài hát đã làm em xao động</p>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h6 class="d-inline">Nội dung:</h6>
+                                    <p class="d-inline">Anh và em yêu nhau thật tình cờ.Louiwf bài hát đã làm em xao động</p>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h6 class="d-inline">Tác giả</h6>
+                                    <p class="d-inline">Nguyễn Văn Giả</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <!-- </div> -->
+            </div>
+
+            
+            <div class="row mt-5">
+                <hr>
+                <div class="footer h3 text-center">
+                    TLU'S MUSIC GARDEN
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</body>
+</html>
---
+++ BTTH01_CSE485_ex02/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div class="container-fluid ">
+        <div class="row border border-info">
+          <?php
+            include 'layout/Trangchu.php'
+          ?>
+
+            <div class="row">
+                <div id="carouselExampleIndicators" class="carousel slide">
+                    <div class="carousel-indicators">
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="./img/main.PNG" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="./img/logo.PNG" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="./img/main.PNG" class="d-block w-100" alt="...">
+                      </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
+            </div>
+
+
+            <div class="row">
+                <div class="main h3 text-center text-uppercase">
+                     <div class="title ">Top bài hát yeu thích</div>
+                </div>
+            </div>
+            
+            <hr>
+
+            <div class="row">
+                <div class="footer h3 text-center">
+                    TLU'S MUSIC GARDEN
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</body>
+</html>
